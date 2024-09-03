d59f25cfa3840372f5198663b68245ee3f782678
FelipeNLaguna <pinholaguna@gmail.com>
2024-09-03 15:01:14 -0300

    Commit cinco

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 // no primeiro plano , vai ser tudo no arquivo main , depois eu separo em arquivos diferentes 
 /*Funções:
-    - Cria arquivo ;
+    - Cria arquivo(Check) ;
     - Ler arquivo ;
     - Inputs usuario : ler/escrever , anime/manga , parar , nome_arquivo , nome_obra (Provavelmente vai estar em várias funções) ; 
     - Tudo é minusculo/maisculo p/ leitura
@@ -27,7 +27,49 @@ using namespace std;
 
 
 */
-
+// classe manga usada para Esrever mangas no arquivo
+class Manga{
+    // Falta lidar com a forma de acesso - Public 
+    // variaveis
+    public:
+        string name;
+        int chapter;
+    
+        void Escreve_manga(string nome_arq){
+            // Abre arq em modo append(adiciona o conteudo no final sem sobrescrever)
+            ofstream arquivo(nome_arq, ios::app);
+            if(!arquivo){
+                // cerr emite msg de erro
+                cerr << "Erro ao abrir o arquivo"<<endl;
+                return;
+            }
+            // Ex: (M) Tonikaku kawaii - ch140
+            arquivo << "(M) " << name << " - " << "ch" << chapter << endl;
+            arquivo.close();
+        }
+};
+// classe anime usada para escrever animes no arquivo
+class Anime{
+    // Falta lidar com a forma de acesso - Public 
+    // variaveis
+     public:
+        string name;
+        int Temp;
+        int episode;
+        
+        void Escreve_anime(string nome_arq){
+            // Abre arq em modo append(adiciona o conteudo no final sem sobrescrever)
+            ofstream arquivo(nome_arq, ios::app);
+            if(!arquivo){
+                // cerr emite msg de erro
+                cerr << "Erro ao abrir o arquivo"<<endl;
+                return;
+            }
+            // Ex: (A) Haikyu - tp1 ep42
+            arquivo << "(A) " << name << " - " <<"tp" << Temp  << " ep" << episode << endl;
+            arquivo.close();
+        }
+};
 // Função responsável por testar se o arquivo existe
 bool Existe_arquivo(string nome_arq){
     // declara um arquivo do tipo ifstream que recebe como nome o nome do arquivo
@@ -93,9 +135,80 @@ int main (){
     }
 
     // Abrir arquivo e escrever e percorre-lo
+    // variavel para manter o looping , se é true continua o while
+    bool still;
+    still = true ;
+    while(still){
+        // Pergunta oq o usuário deseja fazer
+        cout << "Se deseja Escrever Manga , Digite 0!" << endl;
+        cout << "Se deseja Escrever Anime, Digite 1!" << endl;
+        cout << "Se deseja Parar de escrever , Digite 2!" << endl; 
+        // variavel do switch case
+        int write;
+        cin >> write;    
+        switch(write){
+            case 0: {
+                // criar objeto manga
+                // Variaveis para o manga
+                Manga manga;
+                string nome_manga;
+                int capitulo;
+                cout << "Digite o nome do Manga" << endl;
+                cin >> nome_manga;
+                cout << "Digite o capitulo que tu parou" << endl;
+                cin >> capitulo;
+                manga.name = nome_manga;
+                manga.chapter = capitulo;
+                manga.Escreve_manga(nome_arquivo);
 
+                break;
+            }
+            case 1:{
+                // criar objeto anime
+                // variaveis para o anime
+                Anime anime;
+                string nome_anime;
+                int temporada;
+                int episodio;
+                cout << "Digite o nome do Anime " << endl;
+                cin >> nome_anime;
+                cout << "Digite em qual temporada tu parou" << endl;
+                cin >> temporada;
+                cout << "Digite em qual episodio tu parou " << endl;
+                cin >> episodio;
+                anime.name = nome_anime;
+                anime.Temp = temporada;
+                anime.episode = episodio;
+                anime.Escreve_anime(nome_arquivo);
+                break;
+            }
+            case 2 :{
+                // encerra o looping
+                still = false;
+                break;
+            }
+            default:
+                // o user digitou nenhuma das opções dadas 
+                cout << "Opcao invalida , por favor tente novamente !" << endl;
+                break;
+        }
+
+
+    }
 
 
     return 0;
 }
 
+// OJETIVOS :
+// Terminar a classe;
+// Testar se escreve no arquivo
+// Salvar no git
+// Percorrer o arquivo para atualizações
+// Testar e salvar
+// Looping de escrever
+// Testar e salvar
+// Anime temporada
+// Dá erro quando é usado espaço
+
+// Dá para só usar touper para testar?
